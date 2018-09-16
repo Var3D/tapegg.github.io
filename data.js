172ad61c397a4806e4999f19da7e2337f3d019d1
Var3D <aijiuziji@126.com>
2018-09-16 18:08:49 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,7 @@
 var ks=[
-{name:"贪玩蛇战方块",name_en:"Snake Brick",img:"/games/snakebrick-ad.png",iosid:1403252096},
-{name:"切胖次",name_en:"Cut Pants",img:"/games/catpants-ad.png",iosid:1374746799}
+  {name:"贪玩蛇战方块",name_en:"Snake Brick",img:"/games/snakebrick-ad.png",iosid:1403252096},
+  {name:"切胖次",name_en:"Cut Pants",img:"/games/catpants-ad.png",iosid:1374746799},
+  {name:"我们的世界",name_en:"Our Craft",img:"/games/ourcraft-ad.jpg",iosid:1144041654},
+  {name:"英雄大作战",name_en:"Hero Fighting",img:"games/ad-hero.jpg",iosid:1344510227},
+  {name:"皮皮虾大作战",name_en:"King Greedy Snake",img:"/games/snake3d-ad.jpg",iosid:1249822516},
 ];
