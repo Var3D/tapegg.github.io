c207c576fc57b0d4e3ab5eeec6e5d9c4cb884d40
Var3D <aijiuziji@126.com>
2019-09-13 08:05:23 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var ks=[
   {name:"小小坦克大作战",name_en:"Battlefield Tank",img:"/games/tankbattlefield-ad.jpg",iosid:1452175303},
+  {name:"舔狗大作战",name_en:"Follower Town",img:"/games/town-ad.jpg",iosid:1458557639},
   {name:"贪玩蛇战方块",name_en:"Snake Brick",img:"/games/snakebrick-ad.jpg",iosid:1403252096},
   {name:"疯狂切内裤",name_en:"Cut Pants",img:"/games/catpants-ad.jpg",iosid:1374746799},
-  {name:"舔狗大作战",name_en:"Follower Town",img:"/games/town-ad.jpg",iosid:1458557639},
   {name:"我们的世界",name_en:"Our Craft",img:"/games/ourcraft-ad.jpg",iosid:1144041654},
   {name:"英雄大作战",name_en:"Hero Fighting",img:"games/hero-ad-en.jpg",iosid:1344510227},
   {name:"蛇精病大作战",name_en:"King Greedy Snake",img:"/games/snake3d-ad.jpg",iosid:1249822516},
