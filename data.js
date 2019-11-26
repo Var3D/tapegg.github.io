3a063c70fbac7fa0515aa5382e9198d4ac025316
Var3D <aijiuziji@126.com>
2019-11-26 18:29:13 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var ks=[
   {name:"小小坦克大作战",name_en:"Battlefield Tank",img:"/games/tankbattlefield-ad.jpg",iosid:1452175303,google:"com.tapegg.rustbattle.android",taptap:"170850"},
-  {name:"舔狗大作战",name_en:"Follower Town",img:"/games/town-ad.jpg",iosid:1458557639},
+  {name:"舔狗大作战",name_en:"Follower Town",img:"/games/town-ad.jpg",iosid:1458557639,google:"com.tapegg.rustbattle.android"},
   {name:"贪玩蛇战方块",name_en:"Snake Brick",img:"/games/snakebrick-ad.jpg",iosid:1403252096},
   {name:"疯狂切内裤",name_en:"Cut Pants",img:"/games/catpants-ad.jpg",iosid:1374746799},
   {name:"我们的世界",name_en:"Our Craft",img:"/games/ourcraft-ad.jpg",iosid:1144041654},
