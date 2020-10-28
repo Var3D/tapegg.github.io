2ebcf4b87b54e8ad3446b75a0a1e48206cf227b9
Var3D <aijiuziji@126.com>
2020-10-28 16:32:06 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 var ks=[
   {name:"小小坦克大作战",name_en:"Battlefield Tank",nid:"0",img:"/games/tankbattlefield-ad.jpg",iosid:1452175303,google:"com.tapegg.rustbattle",taptap:"170850"},
   {name:"靓仔大作战",name_en:"Hero Fighting",nid:"1",img:"games/hero-ad-en.jpg",iosid:1344510227,google:"com.tapegg.hero",taptap:"181476"},
+  {name:"蛇精病大作战",name_en:"King Greedy Snake",nid:"6",img:"/games/snake3d-ad.jpg",iosid:1249822516,google:"com.tapegg.snake"},
   {name:"舔狗大作战",name_en:"Follower Town",nid:"2",img:"/games/town-ad.jpg",iosid:1458557639},
   {name:"贪玩蛇战方块",name_en:"Snake Brick",nid:"3",img:"/games/snakebrick-ad.jpg",iosid:1403252096},
   {name:"疯狂切内裤",name_en:"Cut Pants",nid:"4",img:"/games/catpants-ad.jpg",iosid:1374746799},
   {name:"我们的世界",name_en:"Our Craft",nid:"5",img:"/games/ourcraft-ad.jpg",iosid:1144041654},
-  {name:"蛇精病大作战",name_en:"King Greedy Snake",nid:"6",img:"/games/snake3d-ad.jpg",iosid:1249822516,google:"com.tapegg.snake"},
   {name:"战车大逃杀",name_en:"Hot Chariot",nid:"7",img:"/games/ad-hotchariot.jpg",iosid:1280455730},
   {name:"像素高速",name_en:"Pixel Highway",nid:"8",img:"/games/colorcar-ad.jpg",iosid:1107836993},
   {name:"天天泡泡龙",name_en:"Bubble Line",nid:"9",img:"/games/bubble-ad.jpg",iosid:1227560990},
