35be10ffa968fef015f2f74781b4a0727ed83362
Var3D <aijiuziji@126.com>
2022-05-23 12:01:33 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 var ks=[
+  {name:"丢雪球大作战",name_en:"Snowball throwing battle",nid:"7",img:"/games/snow-ad.jpg",iosid:1624877392},
   {name:"小小坦克大作战",name_en:"Battlefield Tank",nid:"0",img:"/games/tankbattlefield-ad.jpg",iosid:1452175303,google:"com.tapegg.rustbattle"},
   {name:"靓仔大作战",name_en:"Hero Fighting",nid:"1",img:"games/hero-ad-en.jpg",iosid:1344510227,google:"com.tapegg.hero"},
   {name:"蛇精病大作战",name_en:"King Greedy Snake",nid:"6",img:"/games/snake3d-ad.jpg",iosid:1249822516,google:"com.tapegg.snake"},
